0fa2bae4ccd2a609946ebb9d3b01808f81541155
dgusfr <diegogustavo.fr@gmail.com>
2024-07-19 10:39:41 -0300

    changes

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './rotas/Home';
 import reportWebVitals from './reportWebVitals';
-import { createGlobalStyle } from 'styled-components'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Header from './componentes/Header'
+import { createGlobalStyle } from 'styled-components';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './componentes/Header';
 import Favoritos from './rotas/Favoritos';
 
 const GlobalStyle = createGlobalStyle`
@@ -25,9 +25,7 @@ const GlobalStyle = createGlobalStyle`
   li {
     list-style: none;    
   }
-
-
-`
+`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -36,14 +34,11 @@ root.render(
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/favoritos" element={<Favoritos/>} />
+        <Route path="/favoritos" element={<Favoritos />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
